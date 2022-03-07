d3f91823d6c0fa548d05679bd6d6c2b63ed31c74
Niyati-Sanjay Bafna <nbafna@jones-6.coli.uni-saarland.de>
2022-03-07 13:02:35 +0100

    Reversing some changes that broke the retriever

--- baselines/DPR/run_baseline_retriever.sh
+++ baselines/DPR/run_baseline_retriever.sh
@@ -3,6 +3,6 @@ python dense_retriever.py \
     --ctx_file enwiki_20190201_w100.tsv --n-docs 100 \
     --qa_file ../../data/translate_test_xor_engspan_dev/xor_retrieve_eng_span_dev_google_trans_q.jsonl \
     --encoded_ctx_file â"models/checkpoints/checkpoints_multilingual/embeddings_multi/wiki_emb_*" \
-    --out_file testing1.ret \
+    --out_file baseline_retriever_output.json \
     --validation_workers 4 --batch_size 64 
 
